412f29f4d7174ca2e96128883dd8d2f71e25a2d6
griffnsh <memvivexp@gmail.com>
2024-02-24 19:30:44 -0600

    base project setup and instruction comments made

---
+++ script.ps1
@@ -0,0 +1,5 @@
+# If you would like to add/remove packages run this in your terminal to view your packages 
+# Get-AppxPackage | Select Name, PackageFullName
+
+# Format for removing packages should you want to trim or add to this list
+# Get-AppxPackage **| Remove-AppxPackage
