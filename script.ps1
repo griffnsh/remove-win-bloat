5b4085d9f490366eddbe3755fa1fe739b9e359a4
griffnsh <memvivexp@gmail.com>
2024-02-24 19:39:54 -0600

    getting packages and piping them to be deleted

--- script.ps1
+++ script.ps1
@@ -3,3 +3,16 @@
 
 # Format for removing packages should you want to trim or add to this list
 # Get-AppxPackage **| Remove-AppxPackage
+
+Get-AppxPackage *Microsoft.ZuneMusic*| Remove-AppxPackage
+Get-AppxPackage *Microsoft.XboxGameCallableUI*| Remove-AppxPackage
+Get-AppxPackage *Microsoft.XboxSpeechToTextOverlay*| Remove-AppxPackage
+Get-AppxPackage *Microsoft.XboxGameOverlay*| Remove-AppxPackage
+Get-AppxPackage *Microsoft.Xbox.TCUI*| Remove-AppxPackage
+Get-AppxPackage *Microsoft.MixedReality.Portal* | Remove-AppxPackage
+Get-AppxPackage *Microsoft.BingNews*| Remove-AppxPackage
+Get-AppxPackage *Microsoft.XboxGamingOverlay*| Remove-AppxPackage
+Get-AppxPackage *Clipchamp.Clipchamp*| Remove-AppxPackage
+Get-AppxPackage *Microsoft.MicrosoftSolitaireCollection*| Remove-AppxPackage
+Get-AppxPackage *Microsoft.GamingApp*| Remove-AppxPackage
+Get-AppxPackage *Microsoft.SkypeApp*| Remove-AppxPackage
