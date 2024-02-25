36f2c258b0b5eb3f90851addfd88e5db08e3cb76
griffnsh <memvivexp@gmail.com>
2024-02-24 21:14:04 -0600

    added delete statements for Xbox packages

--- remove-win-bloat.ps1
+++ remove-win-bloat.ps1
@@ -6,6 +6,7 @@
 
 
 # Removing General Bloat
+
 Get-AppxPackage *Microsoft.ZuneMusic*| Remove-AppxPackage
 Get-AppxPackage *Microsoft.MixedReality.Portal* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.BingNews*| Remove-AppxPackage
@@ -13,4 +14,16 @@ Get-AppxPackage *Clipchamp.Clipchamp*| Remove-AppxPackage
 Get-AppxPackage *Microsoft.MicrosoftSolitaireCollection*| Remove-AppxPackage
 Get-AppxPackage *Microsoft.GamingApp*| Remove-AppxPackage
 Get-AppxPackage *Microsoft.SkypeApp*| Remove-AppxPackage
+Get-AppxPackage *Microsoft.ZuneVideo*| Remove-AppxPackage
 
+# Removing Xbox Bloat 
+
+# Finds Xbox related packages on your system !! This removes all Xbox packages except the finnicky XboxGameCableUI
+dism /Online /Get-ProvisionedAppxPackages | Select-String PackageName | Select-String xbox
+
+#  Removes Xbox related packages
+Get-ProvisionedAppxPackage -Online | `
+Where-Object { $_.PackageName -match "xbox" } | `
+ForEach-Object { Remove-ProvisionedAppxPackage -Online -PackageName $_.PackageName }
+
+Get-AppxPackage -AllUsers Microsoft.XboxGamingOverlay | Remove-AppxPackage
